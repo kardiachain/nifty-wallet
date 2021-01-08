7f4346b9233e2842851af5a5ef5a590b58e1544e
phidangnguyen <phidang0210@gmail.com>
2021-01-08 10:54:20 +0700

    pading size ui

--- old-ui/app/keychains/hd/create-vault-complete.js
+++ old-ui/app/keychains/hd/create-vault-complete.js
@@ -25,7 +25,6 @@ class CreateVaultCompleteScreen extends Component {
 
         h('h3.flex-center.section-title', {
           style: {
-            background: '#ffffff',
             color: '#333333',
             marginBottom: 8,
             width: '100%',
