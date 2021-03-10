f9bb77757fe4c9bc953cd5ca36aa0a88bf39204e
phongnhat19 <phongnhat19@gmail.com>
2021-03-10 14:04:45 +0700

    update production endpoint

--- constant.js
+++ constant.js
@@ -1,11 +1,11 @@
 // TESTNET ENDPOINT
-export const API_ENDPOINT = 'https://backend-dev.kardiachain.io/api/v1'
-export const RPC_ENDPOINT = 'https://dev-1.kardiachain.io'
-export const EXPLORER_ENDPOINT = 'https://explorer-dev.kardiachain.io'
+// export const API_ENDPOINT = 'https://backend-dev.kardiachain.io/api/v1'
+// export const RPC_ENDPOINT = 'https://dev-1.kardiachain.io'
+// export const EXPLORER_ENDPOINT = 'https://explorer-dev.kardiachain.io'
 
-// export const API_ENDPOINT = 'https://backend.kardiachain.io/api/v1'
-// export const RPC_ENDPOINT = 'https://rpc.kardiachain.io'
-// export const EXPLORER_ENDPOINT = 'https://explorer.kardiachain.io'
+export const API_ENDPOINT = 'https://backend.kardiachain.io/api/v1'
+export const RPC_ENDPOINT = 'https://rpc.kardiachain.io'
+export const EXPLORER_ENDPOINT = 'https://explorer.kardiachain.io'
 
 export const KRC20ABI = [
   {
